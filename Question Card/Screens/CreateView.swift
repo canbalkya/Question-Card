70ad50e2a4fe7b3aee77ba8257ae1d8f80176142
Can Balkaya <canbalkya@gmail.com>
2020-04-04 17:48:49 +0300

    Developed CreateView.

---
+++ Question Card/Screens/CreateView.swift
@@ -0,0 +1,71 @@
+//
+//  CreateView.swift
+//  Question Card
+//
+//  Created by Can Balkaya on 4/4/20.
+//  Copyright © 2020 Can Balkaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    @State private var title = ""
+    @State private var description = ""
+    @State private var question = ""
+    @State private var answers = ["", "", "", ""]
+    @State private var trueAnswer = 0
+    var questions = Array(1...10)
+    
+    static let trueAnswers = ["First", "Second", "Third", "Fourth"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Title", text: $title)
+                    TextField("Description", text: $description)
+                }
+                
+                    ForEach(questions, id: \.self) { number in
+                        Section(header: Text("\(number). Question")) {
+                            TextField("Question", text: self.$question)
+
+                            TextField("Answer", text: self.$answers[0])
+                            TextField("Answer", text: self.$answers[1])
+                            TextField("Answer", text: self.$answers[2])
+                            TextField("Answer", text: self.$answers[3])
+                            
+                            Picker("True Answer", selection: self.$trueAnswer) {
+                                ForEach(Self.trueAnswers, id: \.self) {
+                                    Text($0)
+                                }
+                            }
+                        }
+                    }
+                
+                    Section {
+                        Button(action: {
+                            
+                        }) {
+                            Text("Add new question")
+                        }
+                    }
+                    
+                    Section {
+                        Button(action: {
+                            
+                        }) {
+                            Text("Create")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+struct CreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateView()
+    }
+}
